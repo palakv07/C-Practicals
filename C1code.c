31a76dbbce76dc012a1cd580bb0260b2ee3ab881
Palak Verma <pv859596@gmail.com>
2026-01-30 11:38:09 +0530

    Add files via upload

---
+++ C1code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    /*int x;
+    int y;
+    printf("number:");
+    scanf("%d""%d",&x , &y);
+    int z = 0;
+    z = x+y;
+    printf("%d",z);*/
+    char[11] s = "PALAK VERMA";
+    int d = 20;
+    int m = 07;
+    int y = 2007;
+    int mobile = 9539899999;
+    printf("%s",s);
+    return 0;
+}
